d707ab96f1ea0495246b938b430aae2c10493419
Max Roos <71812384+MaxRoos04@users.noreply.github.com>
2026-03-30 10:12:27 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hellow fellow student! HaHaHA", "v": "0.1" }
-
-
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
-
-
-from fastapi import FastAPI, Request
-
-app = FastAPI()
+    return { "msg": "Hello local docker"}
 
 @app.get("/api/ip")
-async def get_ip(request: Request):
-    return {"ip": request.client.host}
+def ip(request: Request):
+    return { "ip": request.client.host}
+
+
+@app.get("/ip", response_class=HTMLResponse)
+def ip(request: Request):
+    return f"<h1>Din ip är {request.client.host}</h1>"
+
+@app.get("/hello")
+def hello():
+    return { "msg": "Hello Max"}
